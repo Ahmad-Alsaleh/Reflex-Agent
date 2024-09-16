dec46861523684354a563c247aa945697faf8a8d
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2024-09-17 02:43:28 +0400

    refactor: remove the option of having random goal and agent positions

--- configs.py
+++ configs.py
@@ -1,13 +1,7 @@
-import random
-from typing import Literal, Self
+from typing import Self
 
 from pydantic import model_validator
 from pydantic_settings import BaseSettings
-from rich import print as pprint
-
-
-def _get_random_point(num_of_cells: int) -> tuple[int, int]:
-    return tuple(random.randrange(num_of_cells) for _ in range(2))
 
 
 class _Settings(BaseSettings):
@@ -17,52 +11,32 @@ class _Settings(BaseSettings):
     grid_thickness: int
     window_size: int
     num_of_cells: int
-    goal_position: tuple[int, int] | Literal["random"]
+    goal_position: tuple[int, int]
     goal_color: str
-    agent_init_position: tuple[int, int] | Literal["random"]
+    agent_init_position: tuple[int, int]
 
     @property
     def cell_size(self) -> int:
         return self.window_size / self.num_of_cells
 
     @model_validator(mode="after")
-    def validate_CELL_SIZE(self) -> Self:
-        if self.goal_position == "random":
-            self.goal_position: tuple[int, int] = _get_random_point(self.num_of_cells)
-            while self.agent_init_position == self.goal_position:
-                self.goal_position = _get_random_point(self.num_of_cells)
-            pprint(
-                f"[bold blue]INFO:[/] Using random `GOAL_POSITION = {self.goal_position}`"
-            )
-        elif not all(0 <= coord < self.num_of_cells for coord in self.goal_position):
+    def validate_agent_position(self) -> Self:
+        if not all(0 <= coord < self.num_of_cells for coord in self.goal_position):
             raise ValueError(
                 f"GOAL_POSITION = {self.goal_position} is out of bounds. "
                 f"Should be between 0 and {self.num_of_cells - 1}"
             )
-
-        if self.agent_init_position == "random":
-            self.agent_init_position: tuple[int, int] = _get_random_point(
-                self.num_of_cells
-            )
-            while self.agent_init_position == self.goal_position:
-                self.agent_init_position = _get_random_point(self.num_of_cells)
-            pprint(
-                f"[bold blue]INFO:[/] Using random `AGENT_INIT_POSITION = {self.agent_init_position}`"
-            )
-        elif not all(
+        if not all(
             0 <= coord < self.num_of_cells for coord in self.agent_init_position
         ):
             raise ValueError(
-                f"AGENT_INIT_POSITION = {self.agent_init_position} is out of bounds. "
+                f"`AGENT_INIT_POSITION` = {self.agent_init_position} is out of bounds. "
                 f"Should be between 0 and {self.num_of_cells - 1}"
             )
-
-        if self.goal_position == self.agent_init_position:
+        if self.agent_init_position == self.goal_position:
             raise ValueError(
-                f"GOAL_POSITION and AGENT_INIT_POSITION cannot be the same. "
-                f"GOAL_POSITION = {self.goal_position}, AGENT_INIT_POSITION = {self.agent_init_position}"
+                "`AGENT_INIT_POSITION` and `GOAL_POSITION` cannot be the same."
             )
-
         return self
 
     class Config:

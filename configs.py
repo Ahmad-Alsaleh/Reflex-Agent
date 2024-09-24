7eb24495c5779e39bdf5d29e640ca8399dedf50f
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2024-09-24 19:45:18 +0400

    feat: draw obstacles

--- configs.py
+++ configs.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from typing import Self
 
 from pydantic import model_validator
@@ -14,28 +15,37 @@ class _Settings(BaseSettings):
     goal_position: tuple[int, int]
     goal_color: str
     agent_init_position: tuple[int, int]
+    obstacles_positions: list[tuple[int, int]]
+    obstacles_color: str
 
     @property
     def cell_size(self) -> float:
         return self.window_size / self.num_of_cells
 
     @model_validator(mode="after")
-    def validate_agent_position(self) -> Self:
+    def validate_positions(self) -> Self:
         if not all(-1 <= coord <= self.num_of_cells for coord in self.goal_position):
             raise ValueError(
-                f"GOAL_POSITION = {self.goal_position} is out of bounds. "
-                f"Should be between 0 and {self.num_of_cells - 1}"
+                f"`GOAL_POSITION={self.goal_position}` is out of bounds. "
+                f"Should be between -1 and {self.num_of_cells}"
             )
         if not all(
             0 <= coord < self.num_of_cells for coord in self.agent_init_position
         ):
             raise ValueError(
-                f"`AGENT_INIT_POSITION` = {self.agent_init_position} is out of bounds. "
+                f"`AGENT_INIT_POSITION={self.agent_init_position}` is out of bounds. "
                 f"Should be between 0 and {self.num_of_cells - 1}"
             )
         if self.agent_init_position == self.goal_position:
             raise ValueError(
                 "`AGENT_INIT_POSITION` and `GOAL_POSITION` cannot be the same."
+            )
+        if not all(
+            0 <= coord < self.num_of_cells
+            for coord in chain.from_iterable(self.obstacles_positions)
+        ):
+            raise ValueError(
+                f"All values in `OBSTACLES_POSITIONS` should be between 0 and {self.num_of_cells - 1}."
             )
         return self
 
--- map.py
+++ map.py
@@ -1,19 +1,22 @@
 import turtle
+from enum import Enum, auto
 
 from configs import settings
 
-type Pen = turtle.Turtle
+# type alias
+Pen = turtle.Turtle
+
+
+class PenType(Enum):
+    BOX = auto()
+    LINE = auto()
 
 
 def draw_map() -> None:
     _init_screen()
-
-    grid_pen = _create_drawing_pen(settings.grid_color)
-    _draw_grid(grid_pen)
-
-    goal_pen = _create_drawing_pen(settings.goal_color, is_goal=True)
-    _draw_goal(goal_pen)
-
+    _draw_grid()
+    _draw_obstacles()
+    _draw_goal()
     _finish_drawing()
 
 
@@ -23,20 +26,23 @@ def _init_screen() -> None:
     turtle.tracer(0)
 
 
-def _create_drawing_pen(pen_color: str, is_goal: bool = False) -> Pen:
+def _create_drawing_pen(pen_color: str, pen_type: PenType) -> Pen:
     pen = turtle.Turtle()
     pen.speed(0)
     pen.color(pen_color)
-    if is_goal:
+    if pen_type == PenType.BOX:
         pen.shape("square")
         pen.shapesize(settings.cell_size / 20, settings.cell_size / 20)
-    else:
+    elif pen_type == PenType.LINE:
         pen.pensize(settings.grid_thickness)
+    else:
+        raise ValueError("Invalid value for `pen_type`.")
     pen.hideturtle()
     return pen
 
 
-def _draw_grid(pen: Pen) -> None:
+def _draw_grid() -> None:
+    pen = _create_drawing_pen(settings.grid_color, PenType.LINE)
     for i in range(settings.num_of_cells + 1):
         position = -settings.window_size / 2 + i * settings.cell_size
         _draw_horizontal_line(pen, position)
@@ -57,10 +63,10 @@ def _draw_vertical_line(pen: Pen, position: float) -> None:
     pen.goto(position, settings.window_size / 2)
 
 
-def _draw_goal(pen: Pen) -> None:
+def _fill_cell(pen: Pen, cell_position: tuple[int, int]) -> None:
     x, y = [
         (coord - settings.num_of_cells // 2) * settings.cell_size
-        for coord in settings.goal_position
+        for coord in cell_position
     ]
     if settings.num_of_cells % 2 == 0:
         x += settings.cell_size / 2
@@ -68,6 +74,17 @@ def _draw_goal(pen: Pen) -> None:
     pen.penup()
     pen.goto(x, y)
     pen.stamp()
+
+
+def _draw_goal() -> None:
+    pen = _create_drawing_pen(settings.goal_color, PenType.BOX)
+    _fill_cell(pen, settings.goal_position)
+
+
+def _draw_obstacles() -> None:
+    pen = _create_drawing_pen(settings.obstacles_color, PenType.BOX)
+    for obstacle_position in settings.obstacles_positions:
+        _fill_cell(pen, obstacle_position)
 
 
 def _finish_drawing() -> None:
